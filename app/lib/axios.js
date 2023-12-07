53d4e00cbda742ed0812f312f3ea33696423eac7
Argie Kastro <argiesisidekastro@gmail.com>
2023-12-07 15:41:33 +0800

    added new

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-   baseURL: 'https://self-raised-superst.000webhostapp.com',
-
+    baseURL: `https://self-raised-superst.000webhostapp.com:8000`,
+    //baseURL: `http://127.0.0.1:8000`,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
